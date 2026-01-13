e05a24f0ae6eaf4a4f29107705d9bb611eeb0529
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-01-13 18:26:17 +0530

    Create nodeErrorLog.sh for logging node errors
    Initial version of the node error logging script.

---
+++ nodeErrorLog.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+########################
+# Author: Vidit
+# Date: 13-01-26
+#
+# This script logs of a node
+#
+# Version: V1
+########################
+
+#set -x # Uncomment for Debug Mode
+set -e # Exit the script when error occurs
+set -o pipefail # Pipefail
+
+
+# Uncomment below line after adding the link
+#curl <link of error storage> | grep ERROR
