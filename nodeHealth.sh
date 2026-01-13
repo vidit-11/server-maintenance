297698d291bc445104d94476a609b09aa8d52b9f
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-01-13 18:27:22 +0530

    Add node health check script
    This script outputs the health status of a node, including disk space, memory, CPU, and running processes.

---
+++ nodeHealth.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+########################
+# Author: Vidit
+# Date: 13-01-26
+#
+# This script outputs the health of a node
+#
+# Version: V1
+########################
+
+#set -x # Uncomment for Debug Mode
+set -e # Exit the script when error occurs
+set -o pipefail # Pipefail
+
+# set -exo pipefail
+
+
+echo "=====Disk Space====="
+df -h #gives disk space
+
+echo "=====Memory====="
+free -g #gives memory
+
+echo "=====CPU====="
+nproc #gives resources
+
+echo "=====Running Processes====="
+ps -ef | awk -F" " '{print $2, $8}'
