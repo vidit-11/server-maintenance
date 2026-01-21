9e31b355f2c5195193ec182968c75fcb1d874bee
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-01-21 18:12:21 +0530

    Update nodeHealth.sh

--- nodeHealth.sh
+++ nodeHealth.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-########################
-# Author: Vidit
-# Date: 13-01-26
-#
-# This script outputs the health of a node
-#
-# Version: V1
-########################
+#############################################
+# Author: Vidit                             #
+# Date: 13-01-26                            #
+#                                           #
+# This script outputs the health of a node  #
+#                                           #
+# Version: V1                               #
+#############################################
 
 #set -x # Uncomment for Debug Mode
 set -e # Exit the script when error occurs
